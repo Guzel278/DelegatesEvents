23ef17e69d078c7d03f99e495563e77e340756ce
Guzel278 <71883716+Guzel278@users.noreply.github.com>
2024-12-17 10:16:04 +0300

    Update FileArgs.cs

--- Delegates_Events/FileArgs.cs
+++ Delegates_Events/FileArgs.cs
@@ -3,12 +3,13 @@ namespace Delegates_Events
 {
 	public class FileArgs :EventArgs
 	{
-		public string FileName { get; set; }
-		public bool Cancel { get; set; } = false;
-        public FileArgs(string fileName)
-        {
+	  public string FileName { get; set; }
+	  public bool Cancel { get; set; } = false;
+   
+          public FileArgs(string fileName)
+          {
             FileName = fileName;
-        }
+          }
     }
 }
 
